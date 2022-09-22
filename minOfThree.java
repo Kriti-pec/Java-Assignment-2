e6c85be31d78eaa606d5fc76a6197d1d06893c1e
Kriti-pec <kriti.bt19ece@pec.edu.in>
2022-09-22 11:19:56 +0530

    updated

--- minOfThree.java
+++ minOfThree.java
@@ -1,19 +1,27 @@
 
 public class minOfThree {
-     public static void main(String[] args) {
-      int num1 = 8;
-      int num2 = 67;
-      int num3 = 81;
-      if(num1<num2 && num2<num3)
-      {
-          System.out.println("Smallest number is "+num1);
+  public static void main(String[] args) {
+    int num1 = 8;
+    int num2 = 67;
+    int num3 = 81;
+    int smallest;
+
+    // find the smallest
+    if (num1 < num2) {
+      if (num3 < num1) {
+        smallest = num3;
+      } else {
+        smallest = num1;
       }
-      else if(num2<num1 && num1<num3)
-      { 
-        System.out.println("Smallest number is "+num2);
-      }
-      else{
-        System.out.println("Smallest number is "+num3);
+    } 
+    else {
+      if (num2 < num1) {
+        smallest = num2;
+      } else {
+        smallest = num3;
       }
     }
+
+    System.out.println(smallest + " is the smallest.");
+  }
 }
